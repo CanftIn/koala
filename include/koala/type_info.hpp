ca53a368d396fdc5a081b72de0b3df381b6284ef
CanftIn <wwc7033@gmail.com>
2022-10-21 16:11:56 +0800

    update type_info

--- include/koala/type_info.hpp
+++ include/koala/type_info.hpp
@@ -15,18 +15,81 @@ namespace koala {
     using BareType_t = typename BareType<T>::type;
   } // namespace detail
 
-  //class TypeInfo {
-  // public:
-  //  constexpr TypeInfo(const std::type_info* t_ti) noexcept
-  //    : _type_info(t_ti)
-  //  {}
-//
-  //  constexpr
-//
-  // private:
-  //  struct UnknownType {};
-  //  const std::type_info* _type_info = &typeid(UnknownType);
-  //};
+  class TypeInfo {
+   public:
+    constexpr TypeInfo(const bool t_is_const,
+                       const bool t_is_reference,
+                       const bool t_is_pointer,
+                       const bool t_is_void,
+                       const bool t_is_arithmetic,
+                       const std::type_info* t_ti,
+                       const std::type_info* t_bare_ti) noexcept
+      : _type_info(t_ti),
+        _bare_type_info(t_bare_ti),
+        _flags((static_cast<unsigned int>(t_is_const) << is_const_flag)
+                + (static_cast<unsigned int>(t_is_reference) << is_reference_flag)
+                + (static_cast<unsigned int>(t_is_pointer) << is_pointer_flag)
+                + (static_cast<unsigned int>(t_is_void) << is_void_flag)
+                + (static_cast<unsigned int>(t_is_arithmetic) << is_arithmetic_flag))
+    {}
+
+    constexpr TypeInfo() noexcept = default;
+
+    bool operator < (const TypeInfo& ti) const noexcept {
+      return _type_info->before(*ti._type_info);
+    }
+
+    constexpr bool operator != (const TypeInfo& ti) const noexcept {
+      return !(operator==(ti));
+    }
+
+    constexpr bool operator != (const std::type_info& ti) const noexcept {
+      return !(operator==(ti));
+    }
+
+    constexpr bool operator == (const TypeInfo& ti) const noexcept {
+      return ti._type_info == _type_info || *ti._type_info == *_type_info;
+    }
+
+    constexpr bool operator == (const std::type_info& ti) const noexcept {
+      return !is_undef() && (*_type_info) == ti;
+    }
+
+    constexpr bool is_const() const noexcept { return (_flags & (1 << is_const_flag)) != 0; }
+    constexpr bool is_reference() const noexcept { return (_flags & (1 << is_reference_flag)) != 0; }
+    constexpr bool is_void() const noexcept { return (_flags & (1 << is_void_flag)) != 0; }
+    constexpr bool is_arithmetic() const noexcept { return (_flags & (1 << is_arithmetic_flag)) != 0; }
+    constexpr bool is_undef() const noexcept { return (_flags & (1 << is_undef_flag)) != 0; }
+    constexpr bool is_pointer() const noexcept { return (_flags & (1 << is_pointer_flag)) != 0; }
+
+    const char* name() const noexcept {
+      if (!is_undef()) {
+        return _type_info->name();
+      } else {
+        return "";
+      }
+    }
+
+    const char* bare_name() const noexcept {
+      if (!is_undef()) {
+        return _bare_type_info->name();
+      } else {
+        return "";
+      }
+    }
+
+   private:
+    struct UnknownType {};
+    [[maybe_unused]] const std::type_info* _type_info = &typeid(UnknownType);
+    [[maybe_unused]] const std::type_info* _bare_type_info = &typeid(UnknownType);
+    static const int is_const_flag = 0;
+    static const int is_reference_flag = 1;
+    static const int is_pointer_flag = 2;
+    static const int is_void_flag = 3;
+    static const int is_arithmetic_flag = 4;
+    static const int is_undef_flag = 5;
+    unsigned int _flags = (1 << is_undef_flag);
+  };
 } // namespace koala
 
 #endif
--- test/type_info_test.cc
+++ test/type_info_test.cc
@@ -3,8 +3,15 @@
 
 #include "koala/type_info.hpp"
  
-struct Base { virtual ~Base() = default; };
-struct Derived : Base {};
+struct Base {
+  virtual ~Base() = default;
+private:
+  std::string _name;
+};
+struct Derived : Base {
+private:
+  std::string _profession;
+};
 
 template<typename U, typename V> constexpr bool same = std::is_same_v<U, V>;
  
@@ -38,10 +45,14 @@ void TypeInfo_Test() {
   const std::type_info& unknowntype_info = typeid(UnknownType);
   std::cout << "unknowntype_info type: " << unknowntype_info.name() << "\n";
 
-  if(typeid(Derived).before(typeid(Base)))
+  if (typeid(Derived).before(typeid(Base)))
     std::cout << "Base goes before Derived in this implementation.\n";
   else
     std::cout << "Derived goes before Base in this implementation.\n";
+
+  if (typeid(Base) != typeid(Derived)) {
+    std::cout << "class `Base` != class `Derived`\n";
+  } 
 }
 
 int main() {
