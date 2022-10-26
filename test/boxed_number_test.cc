ddf7a0cecb1bd376205a3cca9f0302f3da464d4d
CanftIn <wwc7033@gmail.com>
2022-10-26 13:36:59 +0800

    update boxed_number_test

--- test/boxed_number_test.cc
+++ test/boxed_number_test.cc
@@ -3,12 +3,13 @@
 #include <vector>
 #include <gtest/gtest.h>
 
-#include "koala/boxed_value.hpp"
+#include "koala/boxed_number.hpp"
 
 using namespace koala;
 
-TEST(BoxedValue, string) {
-
+TEST(BoxedNumber, get_common_type) {
+  // EXPECT_EQ(BoxedNumber::get_common_type(1, true), BoxedNumber::CommonTypes::T_INT8);
+  // EXPECT_EQ(BoxedNumber::get_common_type(1, false), BoxedNumber::CommonTypes::T_UINT8);
 }
 
 int main(int argc, char **argv) {
--- test/xmake.lua
+++ test/xmake.lua
@@ -12,3 +12,8 @@ target("boxed_value_test")
     add_files("boxed_value_test.cc")
     add_deps("koala")
     add_packages("gtest")
+
+target("boxed_number_test")
+    add_files("boxed_number_test.cc")
+    add_deps("koala")
+    add_packages("gtest")
