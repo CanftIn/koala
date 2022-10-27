98a7645d989bea23588ca2f97bfc192e7a7764d8
CanftIn <wwc7033@gmail.com>
2022-10-27 15:17:01 +0800

    update static string

--- include/koala/utility.hpp
+++ include/koala/utility.hpp
@@ -17,7 +17,35 @@ namespace koala::utility
 
     constexpr size_t size() const noexcept { return _size; }
 
-    
+    constexpr const char* c_str() const noexcept { return _data; }
+
+    constexpr auto begin() const noexcept { return _data; }
+
+    constexpr auto end() const noexcept { return _data + _size; }
+
+    constexpr bool operator == (std::string_view other) const noexcept {
+      auto b1 = begin();
+      const auto e1 = end();
+      auto b2 = other.begin();
+      const auto e2 = other.end();
+
+      if (e1 - b1 != e2 - b2) {
+        return false;
+      }
+
+      while (b1 != e1) {
+        if (*b1 != *b2) {
+          return false;
+        }
+        ++b1;
+        ++b2;
+      }
+      return true;
+    }
+
+    bool operator==(const std::string &t_str) const noexcept {
+      return std::equal(begin(), end(), std::cbegin(t_str), std::cend(t_str));
+    }
 
     const size_t _size;
     const char* _data = nullptr;
