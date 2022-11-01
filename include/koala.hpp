b1093bc6b6fa261dcf57f6d9f62118775c12ff88
CanftIn <wwc7033@gmail.com>
2022-11-01 14:30:27 +0800

    update koala engine

--- include/koala.hpp
+++ include/koala.hpp
@@ -3,4 +3,16 @@
 
 #include "koala/define.hpp"
 #include "koala/boxed_value.hpp"
+#include "koala/koala_engine.hpp"
+
+namespace koala
+{
+  class Koala : public KoalaBasic {
+   public:
+    Koala()
+      : KoalaBasic()
+    { }
+  };
+} // namespace koala
+
 #endif
